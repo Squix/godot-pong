cf73a3ed05ff84bc5be2ab63a359ff69ebdf0074
Squix <squixdev.squix@gmail.com>
2024-12-12 15:11:47 +0100

    Add GameManager and goals to handle scoring

---
+++ game_manager.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var player_score = 0
+var opponent_score = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func add_point(target: String):
+	if target == "player":
+		player_score += 1
+	elif target == "opponent":
+		opponent_score += 1
+	
+	print("Scores: [", player_score, ", ", opponent_score, "]")
+
+func _on_opponent_goal_body_entered(body: Node2D) -> void:
+	add_point("player")
