47dc2ad4957e515e744e54eacca0397db38ad1ec
Squix <squixdev.squix@gmail.com>
2024-12-12 15:54:55 +0100

    Add score UI and bit font

--- game_manager.gd
+++ game_manager.gd
@@ -3,15 +3,19 @@ extends Node
 var player_score = 0
 var opponent_score = 0
 
+@onready var player_score_text: Label = $PlayerScoreText
+@onready var opponent_score_text: Label = $OpponentScoreText
+
 @onready var ball_spawner: Marker2D = %BallSpawner
 
 # Update scores
 func add_point(target: String):
 	if target == "player":
 		player_score += 1
+		player_score_text.text = str(player_score)
 	elif target == "opponent":
 		opponent_score += 1
-	
+		opponent_score_text.text = str(opponent_score)
 	print("Scores: [", player_score, ", ", opponent_score, "]")
 
 # When the player or the opponent score
